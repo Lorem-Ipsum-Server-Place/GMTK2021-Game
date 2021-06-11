2b58091020f094607cac8fd9c129399e1e7317ad
Elliot Hargreaves <Elliot.a.h@outlook.com>
2021-06-12 00:49:29 +0100

    Add comments and double jump

--- Player.gd
+++ Player.gd
@@ -9,6 +9,7 @@ var jump_count = 1
 
 const PLAYER_MOVE_SPEED = 450
 const PLAYER_JUMP_VELOCITY = 1000
+const PLAYER_JUMP_COUNT = 2
 
 const GRAVITY = 60
 
@@ -23,17 +24,20 @@ func _ready():
 #	pass
 
 func get_inputs():
+	# If either direction is pressed, figure out which way we're going
 	if Input.is_action_pressed("game_left") or Input.is_action_pressed("game_right"):
 		if Input.is_action_pressed("game_left") and not Input.is_action_pressed("game_right"):
 			player_velocity.x = -PLAYER_MOVE_SPEED
 		elif Input.is_action_pressed("game_right") and not Input.is_action_pressed("game_left"):
 			player_velocity.x = PLAYER_MOVE_SPEED
+	# otherwise cancel our movement
 	else:
 		player_velocity.x = 0
 	
+	# If we have any jumps left, set our vertical jump velocity so we go up
 	if Input.is_action_just_pressed("game_jump") and jump_count > 0:
 		player_velocity.y = -PLAYER_JUMP_VELOCITY
-		#jump_count -= 1
+		jump_count -= 1
 
 	player_velocity.y += GRAVITY
 
@@ -41,5 +45,10 @@ func get_inputs():
 func _physics_process(delta):
 	get_inputs()
 	
+	var player_vertical_speed = player_velocity.y
+	
 	player_velocity = move_and_slide(player_velocity)
 	
+	# If our vertical velocity has changed then we've probably hit the ground
+	if player_velocity.y != player_vertical_speed and player_vertical_speed > 0:
+		jump_count = PLAYER_JUMP_COUNT
